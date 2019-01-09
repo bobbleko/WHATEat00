2e0481c6f26b459564ceb50050f16ffc8861dac4
anunpong <bobblebleble@gmail.com>
2019-01-09 22:05:17 +0700

    new

--- app/src/main/java/anunpongi/th/ac/rmutl/whateat/AboutMeFragment.java
+++ app/src/main/java/anunpongi/th/ac/rmutl/whateat/AboutMeFragment.java
@@ -1,29 +1,27 @@
 package anunpongi.th.ac.rmutl.whateat;
 
-
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AboutMeFragment extends Fragment {
-
-
-    public AboutMeFragment() {
-        // Required empty public constructor
-    }
-
-
+/**Called when the activity is first created*/
+Button Group1 , Group2 , Group3,Group4,Group5,Group6,Group7,Group8,Group9,Group10,Group11, Group12;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about_me, container, false);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+
+
+
     }
+
 
 }
--- app/src/main/java/anunpongi/th/ac/rmutl/whateat/Group9.java
+++ app/src/main/java/anunpongi/th/ac/rmutl/whateat/Group9.java
@@ -1,35 +1,29 @@
 package anunpongi.th.ac.rmutl.whateat;
 
 
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.MediaController;
-import android.widget.TextView;
-import android.widget.VideoView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MainFragment extends Fragment {
+public class Group9 extends Fragment {
 
 
-    public MainFragment() {
+    public Group9() {
         // Required empty public constructor
     }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater , ViewGroup container ,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        return inflater.inflate( R.layout.fragment_group9 , container , false );
     }
 
 }
